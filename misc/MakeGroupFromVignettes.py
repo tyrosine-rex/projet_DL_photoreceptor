545b8b36155c865a62f11945c6cc9228b09befe3
tyrosine_rex <165-tlahaie@users.noreply.pedago-service.univ-lyon1.fr>
2022-11-04 13:43:04 +0100

    code improvement MakeGroupFromVignettes.py now it's perfect

--- misc/MakeGroupFromVignettes.py
+++ misc/MakeGroupFromVignettes.py
@@ -1,39 +1,48 @@
 #! /usr/bin/env python3
 from os import mkdir, listdir
 from os.path import splitext, exists
-from shutil import copy
+from shutil import copy, rmtree
+from sys import stderr
 from argparse import ArgumentParser, Namespace, RawTextHelpFormatter
 
 
 DESCRIPTION = """
 This script is used to copy and tidy up ommatidia images folder and get a tree-like folder
-which each image is stored into the corresponding categoriy
+which each image is stored into the corresponding category.
 
 usage from the root of this project:
-	./misc/MakeGroupFromVignettes.py -s <path>/data_source/ -d <other_path>/data_destination/
+    ./misc/MakeGroupFromVignettes.py -s <path>/data_source/ -d <other_path>/data_destination/
 
 ex:
 #from
 <path>/data_source/
-	abc_1 def_3 ghi_4 jkl_3 mno_3	
+    abc_1 def_3 ghi_4 jkl_3 mno_3
 
 #and create
 <other_path>/data_destination/
-	├──cat_1/
-	│	abc_1
-	├──cat_3/
-	│	def_3 jkl_3 mno_3
-	└──cat_4/
-		ghi_4
+    ├──cat_1/
+    │	abc_1
+    ├──cat_3/
+    │	def_3 jkl_3 mno_3
+    └──cat_4/
+        ghi_4
 """
 
 
 def create_dir(dest: str) -> None:
-    if not exists(dest):
-        mkdir(dest)
+    """
+    Void function to handle os.mkdir()
+    """
+    if exists(dest):
+        rmtree(dest)
+        print(f"{dest} already exists, it was overwrite", file=stderr)
+    mkdir(dest)
 
 
 def create_groups(source: str) -> dict:
+    """
+    Function that return dict such as keys represent count category and values are files
+    """
     groups = {}
     for file in listdir(source):
         name, ext = splitext(file)
@@ -46,18 +55,22 @@ def create_groups(source: str) -> dict:
     return groups
 
 
-def create_dir_groups(dict_groups: dict, dest: str) -> None:
+def copy_data(dict_groups: dict, source: str, dest: str, prefix: str) -> None:
+    """
+    Void function that create tree-like destination folders and fill them with images
+    from source folder
+    """
+    create_dir(dest)
     for key in dict_groups:
-        create_dir(f"{dest}/cat_{key}")
-
-
-def copy_data(dict_groups: dict, source: str, dest: str) -> None:
-    for key in dict_groups:
+        create_dir(f"{dest}/{prefix}{key}")
         for file in dict_groups[key]:
-            copy(f"{source}/{file}", f"{dest}/cat_{key}/{file}")
+            copy(f"{source}/{file}", f"{dest}/{prefix}{key}/{file}")
 
 
 def handle_arguments() -> Namespace:
+    """
+    Function that handle argparse.ArgumentParser()
+    """
     parser = ArgumentParser(
         prog="MakeGroupFromVignettes",
         description=DESCRIPTION,
@@ -66,26 +79,35 @@ def handle_arguments() -> Namespace:
     parser.add_argument(
         "-s", "--source",
         required=True,
-        help="path of source folder that contain images, must be exists",
+        help="Path of source folder that contain images. Must be exists",
     )
     parser.add_argument(
-        "-d", "--destination", required=True, help="path of destination folder"
+        "-d", "--destination",
+        required=True,
+        help="Path of destination folder. If exists, it will be overwrite"
+    )
+    parser.add_argument(
+        "-p", "--prefix",
+        default="cat_",
+        help="Prefix used for each categories folders. 'cat_' by default"
     )
     return parser.parse_args()
 
 
 def main() -> None:
+    """
+    Main function that coordinate the execution of all others functions
+    """
     args = handle_arguments()
 
     if exists(args.source):
-        create_dir(args.destination)
         grps = create_groups(args.source)
-        create_dir_groups(grps, args.destination)
-        copy_data(grps, args.source, args.destination)
-        print("job is done!")
+        copy_data(grps, args.source, args.destination, args.prefix)
 
     else:
         raise FileNotFoundError(f"{args.source} is not found")
+
+    print("job is done!", file=stderr)
 
 
 if __name__ == "__main__":
