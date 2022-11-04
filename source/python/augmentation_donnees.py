b09aed8d4de8d3c481da5079f012b184a2aeae16
Grégoire ALIZADEH NOIRET <gregoire.alizadehnoiret@gmail.com>
2022-11-04 20:47:39 +0100

    All base images are copied before new ones are generated

--- source/python/augmentation_donnees.py
+++ source/python/augmentation_donnees.py
@@ -9,21 +9,21 @@ import sys
 import shutil
 from PIL import ImageEnhance
 
-def cart2pol(x, y):
-    rho = np.sqrt(x**2 + y**2)
-    phi = np.arctan2(y, x)
-    return (rho, phi)
+# def cart2pol(x, y):
+#     rho = np.sqrt(x**2 + y**2)
+#     phi = np.arctan2(y, x)
+#     return (rho, phi)
 
-def pol2cart(rho, phi):
-    x = rho * np.cos(phi)
-    y = rho * np.sin(phi)
-    return (x, y)
+# def pol2cart(rho, phi):
+#     x = rho * np.cos(phi)
+#     y = rho * np.sin(phi)
+#     return (x, y)
 
-n_img = 100
+n_img = 1000
 classes = [str(i) for i in range(10)]
 experiments = ['training', 'testing']
 img_extension = '.tif'
-augmented_prefix = 'eaug_'
+augmented_prefix = 'ezaug_'
 dim = 60
 global_img_i = 0
 
@@ -36,7 +36,6 @@ else:
 
 print('dir_to_augment:', dir_to_augment)
 
-# expdirs = os.listdir(dir_to_augment)
 expdirs = experiments
 print(expdirs)
 
@@ -46,10 +45,16 @@ for expdir in experiments:
     full_tgt_dir = dir_to_augment+'/'+tgt_dir
     if not tgt_dir in expdirs:
         print('Creating directory:', full_tgt_dir)
-        os.mkdir(full_tgt_dir)
+        try:
+            os.mkdir(full_tgt_dir)
+        except:
+            print('Experiment directory already exists')
         for i in range(10):
             print('Creating directory:', full_tgt_dir+'/'+str(i))
-            os.mkdir(full_tgt_dir+'/'+str(i))
+            try:
+                os.mkdir(full_tgt_dir+'/'+str(i))
+            except:
+                print('Class directory already exists')
     else:
         print('Target directory already exists:', full_tgt_dir)
 
@@ -58,25 +63,33 @@ for expdir in experiments:
     if expdir == 'training':
         print('expdir:', expdir)
 
+        # For each class folder in training set, copy images and generate some if necessary
         for classdir in os.listdir(dir_to_augment+'/'+expdir):
-            full_classdir = dir_to_augment+'/'+expdir+'/'+classdir
-            print(full_classdir)
+            full_source_dir = dir_to_augment+'/'+expdir+'/'+classdir
+            full_dest_dir = dir_to_augment+'/'+augmented_prefix+expdir+'/'+classdir
+            print('source, dest:', full_source_dir, full_dest_dir)
 
-            class_vignettes = os.listdir(full_classdir)
-            # print(len(class_vignettes), 'class_vignettes', class_vignettes)
+            # Generate images
+            class_vignettes = os.listdir(full_source_dir)
+            print(len(class_vignettes), 'class_vignettes')
             if len(class_vignettes) > 0:
                 # Copy all exsiting images into the augmented directory
-                # for img_file in class_vignettes:
-                #     shutil.copyfile(full_classdir)
+                for img_file in class_vignettes:
+                    vignette = Image.open(full_source_dir+'/'+img_file)
+                    final = vignette
+                    final.save(dir_to_augment+'/'+augmented_prefix+experiments[0]+'/'+classdir+'/'+str(global_img_i)+img_extension)
+                    ImageOps.mirror(final).save(dir_to_augment+'/'+augmented_prefix+experiments[1]+'/'+classdir+'/'+str(global_img_i)+img_extension)
+                    # shutil.copyfile(full_source_dir+'/'+img_file, full_dest_dir+'/'+str(global_img_i)+img_extension)
+                    global_img_i += 1
 
-                # As long as images need to be generated, create new ones randomly
-                for i in range(len(class_vignettes), n_img+1):
-                    class_vignettes = os.listdir(full_classdir)
+                # As long as there are not enough images, create new ones randomly
+                for i in range(len(class_vignettes), n_img):
+                    # class_vignettes = os.listdir(full_source_dir)
 
                     idx_choice = r.randint(0, len(class_vignettes)-1)
                     print(i, 'idx_choice', idx_choice)
 
-                    vignette = Image.open(full_classdir+'/'+class_vignettes[idx_choice])
+                    vignette = Image.open(full_source_dir+'/'+class_vignettes[idx_choice])
                     
                     print(len(class_vignettes))
                     print(class_vignettes[idx_choice])
@@ -97,15 +110,6 @@ for expdir in experiments:
                     global_img_i += 1
 
 print('global_img_i:', global_img_i)
-        
-        # for vignette in os.listdir(full_classdir):
-        #     print(vignette)
-
-#     print('expdir', expdir)
-#     for img in os.listdir(expdir):
-#         print('Current image:', img)
-
-
 
 # for experiment in experiments:
 #     # Create testing / training directories if they do not exist
